a462e1a99863a65d04db3da273dad51ac76346a6
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-03-22 18:02:31 -0300

    Add project files.

---
+++ Recursividade/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividade
+{
+    public class Calculadora
+    {
+        public int Multiplica(int num1, int num2)
+        {
+            if (num1 == 0 || num2 == 0) return 0;
+
+            if (num2 == 1) return num1;
+
+            return num1 + (Multiplica(num1, num2 - 1));
+        }
+    }
+}
---
+++ Recursividade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            var result = calc.Multiplica(3, 4);
+
+            Console.WriteLine(result);
+        }
+    }
+}
